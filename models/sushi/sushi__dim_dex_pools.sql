976bf50476fcc21bdc7ede8855e9594378344b22
Vahid-flipside <98771482+Vahid-flipside@users.noreply.github.com>
2022-07-13 14:36:50 -0400

    new table (#9)

--- models/sushi/sushi__dim_dex_pools.sql
+++ models/sushi/sushi__dim_dex_pools.sql
@@ -3,17 +3,18 @@
 ) }}
 
     SELECT
-        pair_contract_address as pool_address,
-        token0_contract_address as token0_address,
+        lower(pair_contract_address) as pool_address,
+        lower(token0_contract_address) as token0_address,
+        token0_symbol||'-'||token1_symbol as pool_name,
         token0_symbol,
         token0_name,
-        tokens1_contract_address as token1_address,
+        lower(token1_contract_address) as token1_address,
         token1_symbol,
-        token0_name,
+        token1_name,
         token0_decimals,
         token1_decimals 
     FROM
          {{ source(
             'arbitrum_pools',
-            'SUSHI_DIM_KASHI_POOLS'
+            'SUSHI_DIM_DEX_POOLS'
         ) }} 
