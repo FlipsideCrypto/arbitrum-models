a678a5e9d997941e516c504ba5d12c9fa26117cf
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-05-18 13:43:05 -0400

    decoding base (#50)

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -2,9 +2,15 @@
     {% if var("UPDATE_UDFS_AND_SPS") %}
         {% set sql %}
         CREATE schema if NOT EXISTS silver;
-    {{ create_js_hex_to_int() }};
-    {{ create_udf_hex_to_int(
+{{ create_js_hex_to_int() }};
+{{ create_udf_hex_to_int(
             schema = "public"
+        ) }}
+        {{ create_udf_keccak(
+            schema = 'silver'
+        ) }}
+        {{ create_udf_simple_event_names(
+            schema = 'silver'
         ) }}
 
         {% endset %}
