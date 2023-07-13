789eeb24d4cb46119b877e2719bae220c138d5b2
drethereum <71602799+drethereum@users.noreply.github.com>
2023-07-13 12:51:55 -0600

    AN-3048/arbitrum-ez-dex-swaps (#88)
    * docs, silver and gold models
    
    * logs2 -> logs
    
    * new pools
    
    * curve fr config
    
    * curve range
    
    * docs

---
+++ models/silver/dex/camelot/silver_dex__camelot_pools.sql
@@ -0,0 +1,60 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = "pool_address"
+) }}
+
+WITH pool_creation AS (
+
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        event_index,
+        contract_address,
+        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
+        CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40)) AS token0,
+        CONCAT('0x', SUBSTR(topics [2] :: STRING, 27, 40)) AS token1,
+        CONCAT('0x', SUBSTR(segmented_data [0] :: STRING, 25, 40)) AS pool_address,
+        _log_id,
+        _inserted_timestamp
+    FROM
+        {{ ref ('silver__logs') }}
+    WHERE
+        contract_address IN (
+            '0x6eccab422d763ac031210895c81787e87b43a652',
+            --v2
+            '0xd490f2f6990c0291597fd1247651b4e0dcf684dd',
+            '0x1a3c9b1d2f0529d97f2afc5136cc23e58f1fd35b'
+        ) --v3
+        AND topics [0] :: STRING IN (
+            '0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9',
+            --PairCreated v2
+            '0x91ccaa7a278130b65168c3a0c8d3bcae84cf5e43704342bd3ec0b59e59c036db'
+        ) --v3
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) :: DATE
+    FROM
+        {{ this }}
+)
+AND pool_address NOT IN (
+    SELECT
+        DISTINCT pool_address
+    FROM
+        {{ this }}
+)
+{% endif %}
+)
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    event_index,
+    token0,
+    token1,
+    pool_address,
+    _inserted_timestamp
+FROM
+    pool_creation
