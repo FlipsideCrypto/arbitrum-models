4ca16e01d51a1eaa0a8ac90ca1a3e78bfd074aa3
mattromano <mattromano88@gmail.com>
2023-07-26 14:27:25 -0700

    fix merge conflicts

--- macros/tests/missing_txs.sql
+++ macros/tests/missing_txs.sql
@@ -33,8 +33,10 @@ FROM
     ON base_block_number = model_block_number
     AND base_tx_hash = model_tx_hash
 WHERE
-    model_tx_hash IS NULL
-    OR model_block_number IS NULL
+    (
+        model_tx_hash IS NULL
+        OR model_block_number IS NULL
+    )
 {% endmacro %}
 
 {% macro recent_missing_txs(
@@ -122,38 +124,23 @@ WHERE
 {% endmacro %}
 
 {% macro missing_traces(
-        model
+        model1,
+        model2
     ) %}
-    WITH txs_base AS (
-        SELECT
-            block_number AS base_block_number,
-            tx_hash AS base_tx_hash
-        FROM
-            {{ ref('test_silver__transactions_full') }}
-        WHERE
-            to_address <> '0x000000000000000000000000000000000000006e'
-            AND block_number > 22207814
-    ),
-    model_name AS (
-        SELECT
-            block_number AS model_block_number,
-            tx_hash AS model_tx_hash
-        FROM
-            {{ model }}
-    )
 SELECT
-    base_block_number,
-    base_tx_hash,
-    model_block_number,
-    model_tx_hash
+    DISTINCT block_number
 FROM
-    txs_base
-    LEFT JOIN model_name
-    ON base_block_number = model_block_number
-    AND base_tx_hash = model_tx_hash
+    {{ model1 }}
+    tx
+    LEFT JOIN {{ model2 }}
+    tr USING (
+        block_number,
+        tx_hash
+    )
 WHERE
-    model_tx_hash IS NULL
-    OR model_block_number IS NULL
+    tr.tx_hash IS NULL
+    AND tx.to_address <> '0x000000000000000000000000000000000000006e'
+    AND tr.to_address <> '0x000000000000000000000000000000000000006e'
 {% endmacro %}
 
 {% macro recent_missing_traces(
--- tests/arbitrum/test_silver__missing_traces.sql
+++ tests/arbitrum/test_silver__missing_traces.sql
@@ -1,2 +1,2 @@
 -- depends_on: {{ ref('test_silver__transactions_full') }}
-{{ missing_traces(ref("test_silver__traces_full")) }}
+{{ missing_traces(ref("test_silver__transactions_full"), ref("test_silver__traces_full")) }}
