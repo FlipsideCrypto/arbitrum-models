2cfd5d3886383791d3c5129494839546d3fc0271
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-08-03 16:05:30 -0400

    remove (#110)

--- models/gold/core/core__ez_eth_transfers.sql
+++ models/gold/core/core__ez_eth_transfers.sql
@@ -12,17 +12,7 @@ WITH eth_base AS (
         to_address,
         eth_value,
         identifier,
-        input,
-        utils.udf_hex_to_int(
-            COALESCE(
-                DATA :value :: STRING,
-                DATA :action :value :: STRING
-            )
-        ) AS eth_value_precise_raw,
-        utils.udf_decimal_adjust(
-            eth_value_precise_raw,
-            18
-        ) AS eth_value_precise
+        input
     FROM
         {{ ref('core__fact_traces') }}
     WHERE
@@ -49,8 +39,6 @@ SELECT
     A.from_address AS eth_from_address,
     A.to_address AS eth_to_address,
     A.eth_value AS amount,
-    A.eth_value_precise_raw AS amount_precise_raw,
-    A.eth_value_precise AS amount_precise,
     ROUND(
         A.eth_value * eth_price,
         2
--- models/gold/core/core__fact_traces.sql
+++ models/gold/core/core__fact_traces.sql
@@ -11,19 +11,6 @@ SELECT
     from_address,
     to_address,
     eth_value,
-    IFNULL(
-        utils.udf_hex_to_int(
-            COALESCE(
-                DATA :value :: STRING,
-                DATA :action :value :: STRING
-            )
-        ),
-        '0'
-    ) AS eth_value_precise_raw,
-    utils.udf_decimal_adjust(
-        eth_value_precise_raw,
-        18
-    ) AS eth_value_precise,
     gas,
     gas_used,
     input,
