92ee015e3513396a49d98597731046f7478b842c
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-08-08 18:21:58 -0400

    fix (#123)

--- models/silver/core/silver__created_contracts.sql
+++ models/silver/core/silver__created_contracts.sql
@@ -20,7 +20,7 @@ WHERE
     AND input IS NOT NULL
     AND input != '0x'
     AND tx_status = 'SUCCESS'
-    AND traces_status = 'SUCCESS'
+    AND trace_status = 'SUCCESS'
 
 {% if is_incremental() %}
 AND _inserted_timestamp >= (
