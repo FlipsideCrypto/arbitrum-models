33562a4d21e3f03ff088af3e474d2f3e1699506b
Austin <austin@flipsidecrypto.com>
2023-09-25 15:18:49 -0400

    updates

--- models/streamline/silver/core/retry/_missing_traces.sql
+++ models/streamline/silver/core/retry/_missing_traces.sql
@@ -2,6 +2,13 @@
     materialized = "ephemeral"
 ) }}
 
+WITH lookback AS (
+
+    SELECT
+        block_number
+    FROM
+        {{ ref("_block_lookback") }}
+)
 SELECT
     DISTINCT tx.block_number block_number
 FROM
@@ -11,17 +18,15 @@ FROM
     tr
     ON tx.block_number = tr.block_number
     AND tx.tx_hash = tr.tx_hash
-    AND tr.block_timestamp >= DATEADD(
-        'day',
-        -2,
-        CURRENT_DATE
-    )
+    AND tr.block_timestamp >= DATEADD('hour', -84, SYSDATE())
 WHERE
-    tx.block_timestamp >= DATEADD(
-        'day',
-        -2,
-        CURRENT_DATE
-    )
+    tx.block_timestamp >= DATEADD('hour', -84, SYSDATE())
     AND tr.tx_hash IS NULL
     AND tx.to_address <> '0x000000000000000000000000000000000000006e'
     AND tr.to_address <> '0x000000000000000000000000000000000000006e'
+    AND tx.block_number >= (
+        SELECT
+            block_number
+        FROM
+            lookback
+    )
