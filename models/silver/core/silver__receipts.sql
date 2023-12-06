0c90cdabff61f07eda1b9cebfdec1f63e4bb4250
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-12-06 09:52:10 -0500

    fr (#176)

--- models/silver/core/silver__receipts.sql
+++ models/silver/core/silver__receipts.sql
@@ -5,7 +5,6 @@
     unique_key = "block_number",
     cluster_by = "ROUND(block_number, -3)",
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION on equality(tx_hash)",
-    full_refresh = false,
     tags = ['core','non_realtime']
 ) }}
 
