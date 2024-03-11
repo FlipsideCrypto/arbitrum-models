4417815702d55ad252e6a12cd55292c47133c21e
mattromano <mattromano88@gmail.com>
2024-03-11 15:52:01 -0700

    fixed perp and spot models

--- models/silver/protocol/vertex/silver__vertex_liquidations.sql
+++ models/silver/protocol/vertex/silver__vertex_liquidations.sql
@@ -6,7 +6,8 @@
     tags = ['curated','reorg']
 ) }}
 
-WITH health_groups as (
+WITH health_groups AS (
+
     SELECT
         product_id,
         health_group,
@@ -21,12 +22,13 @@ logs AS (
         *
     FROM
         {{ ref('silver__logs') }}
-        WHERE
-        topics [0] :: STRING  IN (
+    WHERE
+        topics [0] :: STRING IN (
             '0xf96988bb67b73fa61d64c48dc2e91ae6b697ebb8c5a496d238309aa20fbf6458',
             '0x494f937f5cc892f798248aa831acfb4ad7c4bf35edd8498c5fb431ce1e38b035'
         )
         AND contract_address = '0xae1ec28d6225dce2ff787dcb8ce11cf6d3ae064f'
+
 {% if is_incremental() %}
 AND _inserted_timestamp >= (
     SELECT
@@ -39,7 +41,6 @@ AND _inserted_timestamp >= (
 {% endif %}
 ),
 logs_pull_v1 AS (
-
     SELECT
         block_number,
         block_timestamp,
@@ -74,7 +75,7 @@ logs_pull_v1 AS (
         utils.udf_hex_to_int(
             segmented_data [3] :: STRING
         ) :: INT AS insurance_cover,
-        NULL as is_encoded_spread,
+        NULL AS is_encoded_spread,
         _log_id,
         _inserted_timestamp
     FROM
@@ -84,7 +85,6 @@ logs_pull_v1 AS (
         AND contract_address = '0xae1ec28d6225dce2ff787dcb8ce11cf6d3ae064f'
 ),
 logs_pull_v2 AS (
-
     SELECT
         block_number,
         block_timestamp,
@@ -116,8 +116,8 @@ logs_pull_v2 AS (
         ) :: INT AS amount_quote,
         NULL AS insurance_cover,
         udf_hex_to_int(
-            segmented_data[1]::STRING
-         ) AS is_encoded_spread,
+            segmented_data [1] :: STRING
+        ) AS is_encoded_spread,
         _log_id,
         _inserted_timestamp
     FROM
@@ -126,7 +126,7 @@ logs_pull_v2 AS (
         topics [0] :: STRING = '0x494f937f5cc892f798248aa831acfb4ad7c4bf35edd8498c5fb431ce1e38b035'
         AND contract_address = '0xae1ec28d6225dce2ff787dcb8ce11cf6d3ae064f'
 ),
-v2_vertex_decode as (
+v2_vertex_decode AS (
     SELECT
         block_number,
         block_timestamp,
@@ -146,123 +146,127 @@ v2_vertex_decode as (
         amount_quote,
         insurance_cover,
         CASE
-            WHEN is_encoded_spread = 1 THEN utils.udf_int_to_binary(product_id) 
-            else NULL
-        END as bin_product_ids,
-        CASE 
-            WHEN is_encoded_spread = 1 THEN 
-            ARRAY_CONSTRUCT(
-                utils.udf_binary_to_int(
-                SUBSTR(bin_product_ids, -16)),
-                utils.udf_binary_to_int(
-                SUBSTR(bin_product_ids, 1, GREATEST(LEN(bin_product_ids) - 16, 1))
-                )
-            ) 
-            ELSE NULL 
-        END AS decoded_spread_product_ids,
+            WHEN is_encoded_spread = 1 THEN utils.udf_int_to_binary(product_id)
+            ELSE NULL
+        END AS bin_product_ids,
         CASE
-            WHEN is_encoded_spread = 1 THEN decoded_spread_product_ids[0]::STRING
-            ELSE product_id
-        END AS product_id,
-        _log_id,
-        _inserted_timestamp
-    FROM
-        logs_pull_v2
-),
-FINAL AS (
+            WHEN is_encoded_spread = 1 THEN ARRAY_CONSTRUCT(
+                utils.udf_binary_to_int(SUBSTR(bin_product_ids, -16)),
+                utils.udf_binary_to_int(
+                    SUBSTR(
+                        bin_product_ids,
+                        1,
+                        GREATEST(len(bin_product_ids) - 16, 1))
+                    )
+                )
+                ELSE NULL
+            END AS decoded_spread_product_ids,
+            CASE
+                WHEN is_encoded_spread = 1 THEN decoded_spread_product_ids [0] :: STRING
+                ELSE product_id
+            END AS product_id,
+            _log_id,
+            _inserted_timestamp
+            FROM
+                logs_pull_v2
+        ),
+        FINAL AS (
+            SELECT
+                block_number,
+                block_timestamp,
+                tx_hash,
+                contract_address,
+                event_name,
+                event_index,
+                origin_function_signature,
+                origin_from_address,
+                origin_to_address,
+                digest,
+                trader,
+                subaccount,
+                'v1' AS version,
+                MODE,
+                NULL AS product_id,
+                l.health_group,
+                p.health_group_symbol,
+                amount AS amount_unadj,
+                amount / pow(
+                    10,
+                    18
+                ) AS amount,
+                amount_quote AS amount_quote_unadj,
+                amount_quote / pow(
+                    10,
+                    18
+                ) AS amount_quote,
+                insurance_cover AS insurance_cover_unadj,
+                insurance_cover / pow(
+                    10,
+                    18
+                ) AS insurance_cover,
+                is_encoded_spread,
+                ARRAY_CONSTRUCT(
+                    NULL,
+                    NULL
+                ) AS spread_product_ids,
+                _log_id,
+                _inserted_timestamp
+            FROM
+                logs_pull_v1 l
+                LEFT JOIN health_groups p
+                ON l.health_group = p.health_group
+            UNION ALL
+            SELECT
+                block_number,
+                block_timestamp,
+                tx_hash,
+                contract_address,
+                event_name,
+                event_index,
+                origin_function_signature,
+                origin_from_address,
+                origin_to_address,
+                digest,
+                trader,
+                subaccount,
+                'v2' AS version,
+                MODE,
+                l.product_id,
+                p.health_group,
+                p.health_group_symbol,
+                amount AS amount_unadj,
+                amount / pow(
+                    10,
+                    18
+                ) AS amount,
+                amount_quote AS amount_quote_unadj,
+                amount_quote / pow(
+                    10,
+                    18
+                ) AS amount_quote,
+                NULL AS insurance_cover_unadj,
+                insurance_cover,
+                CASE
+                    WHEN is_encoded_spread = 1 THEN TRUE
+                    ELSE FALSE
+                END AS is_encoded_spread,
+                decoded_spread_product_ids AS spread_product_ids,
+                _log_id,
+                _inserted_timestamp
+            FROM
+                v2_vertex_decode l
+                LEFT JOIN health_groups p
+                ON l.product_id = p.product_id
+        )
     SELECT
-        block_number,
-        block_timestamp,
-        tx_hash,
-        contract_address,
-        event_name,
-        event_index,
-        origin_function_signature,
-        origin_from_address,
-        origin_to_address,
-        digest,
-        trader,
-        subaccount,
-        'v1' as version,
-        MODE,
-        NULL AS product_id,
-        l.health_group,
-        p.health_group_symbol,
-        amount AS amount_unadj,
-        amount / pow(
-            10,
-            18
-        ) AS amount,
-        amount_quote AS amount_quote_unadj,
-        amount_quote / pow(
-            10,
-            18
-        ) AS amount_quote,
-        insurance_cover AS insurance_cover_unadj,
-        insurance_cover / pow(
-            10,
-            18
-        ) AS insurance_cover,
-        is_encoded_spread,
-        ARRAY_CONSTRUCT(NULL,NULL) AS spread_product_ids,
-        _log_id,
-        _inserted_timestamp
+        *,
+        {{ dbt_utils.generate_surrogate_key(
+            ['tx_hash','event_index']
+        ) }} AS vertex_liquidation_id,
+        SYSDATE() AS inserted_timestamp,
+        SYSDATE() AS modified_timestamp,
+        '{{ invocation_id }}' AS _invocation_id
     FROM
-        logs_pull_v1 l
-        LEFT JOIN health_groups p
-        ON l.health_group = p.health_group
-UNION ALL
-    SELECT
-        block_number,
-        block_timestamp,
-        tx_hash,
-        contract_address,
-        event_name,
-        event_index,
-        origin_function_signature,
-        origin_from_address,
-        origin_to_address,
-        digest,
-        trader,
-        subaccount,
-        'v2' as version,
-        MODE,
-        l.product_id,
-        p.health_group,
-        p.health_group_symbol,
-        amount AS amount_unadj,
-        amount / pow(
-            10,
-            18
-        ) AS amount,
-        amount_quote AS amount_quote_unadj,
-        amount_quote / pow(
-            10,
-            18
-        ) AS amount_quote,
-        NULL AS insurance_cover_unadj,
-        insurance_cover,
-        CASE 
-            WHEN is_encoded_spread = 1 THEN TRUE
-            ELSE FALSE
-        END AS is_encoded_spread,
-        decoded_spread_product_ids AS spread_product_ids,
-        _log_id,
-        _inserted_timestamp
-    FROM
-        v2_vertex_decode l
-        LEFT JOIN health_groups p
-        ON l.product_id = p.product_id
-)
-SELECT
-    *,
-    {{ dbt_utils.generate_surrogate_key(
-        ['tx_hash','event_index']
-    ) }} AS vertex_liquidation_id,
-    SYSDATE() AS inserted_timestamp,
-    SYSDATE() AS modified_timestamp,
-    '{{ invocation_id }}' AS _invocation_id
-FROM
-    FINAL qualify(ROW_NUMBER() over(PARTITION BY _log_id
-ORDER BY
-    _inserted_timestamp DESC)) = 1
+        FINAL qualify(ROW_NUMBER() over(PARTITION BY _log_id
+    ORDER BY
+        _inserted_timestamp DESC)) = 1
--- models/silver/protocol/vertex/silver__vertex_spot.sql
+++ models/silver/protocol/vertex/silver__vertex_spot.sql
@@ -6,33 +6,42 @@
     tags = ['curated','reorg']
 ) }}
 
-WITH vertex_products as (
-   
+WITH vertex_products AS (
+
     SELECT
         block_number,
         block_timestamp,
         tx_hash,
-        product_id,
+        product_id :: STRING AS product_id,
         book_address,
         product_type,
         ticker_id,
         symbol,
-        name,
+        NAME,
         version
     FROM
         {{ ref('silver__vertex_dim_products') }}
     WHERE
         product_type = 'spot'
 ),
-logs as (
+logs AS (
     SELECT
         *
     FROM
         {{ ref('silver__logs') }}
     WHERE
-        (contract_address IN (SELECT book_address FROM vertex_products where product_type = 'spot')
-    or
-        contract_address = lower('0xa4369d8e3dc847aedf17f4125f1abb1bc18fc060'))
+        (
+            contract_address IN (
+                SELECT
+                    book_address
+                FROM
+                    vertex_products
+                WHERE
+                    product_type = 'spot'
+            )
+            OR contract_address = LOWER('0xa4369d8e3dc847aedf17f4125f1abb1bc18fc060')
+        )
+
 {% if is_incremental() %}
 AND _inserted_timestamp >= (
     SELECT
@@ -45,7 +54,6 @@ AND _inserted_timestamp >= (
 {% endif %}
 ),
 order_fill_decode AS (
-
     SELECT
         l.block_number,
         l.block_timestamp,
@@ -98,16 +106,13 @@ order_fill_decode AS (
         l._log_id,
         l._inserted_timestamp
     FROM
-        logs
-        l
-        INNER JOIN vertex_products
-        s
+        logs l
+        INNER JOIN vertex_products s
         ON s.book_address = l.contract_address
     WHERE
         topics [0] :: STRING = '0x224253ad5cda2459ff587f559a41374ab9243acbd2daff8c13f05473db79d14c'
 ),
 order_fill_decode_v2 AS (
-
     SELECT
         l.block_number,
         l.block_timestamp,
@@ -121,7 +126,7 @@ order_fill_decode_v2 AS (
         regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
         utils.udf_hex_to_int(
             topics [1] :: STRING
-        )  AS product_id,
+        ) AS product_id,
         s.symbol,
         topics [2] :: STRING AS digest,
         --unique hash of the order
@@ -162,18 +167,14 @@ order_fill_decode_v2 AS (
         l._log_id,
         l._inserted_timestamp
     FROM
-        logs
-        l
-        INNER JOIN vertex_products
-        s
+        logs l
+        INNER JOIN vertex_products s
         ON s.product_id = utils.udf_hex_to_int(
             topics [1] :: STRING
         )
     WHERE
         topics [0] :: STRING = '0x7c57459d6f4f0fb2fc5b1e298c8c0eb238422944964aa1e249eaa78747f0cca9'
-        AND
-            contract_address = lower('0xa4369d8e3dc847aedf17f4125f1abb1bc18fc060')
-
+        AND contract_address = LOWER('0xa4369d8e3dc847aedf17f4125f1abb1bc18fc060')
 ),
 order_fill_format AS (
     SELECT
@@ -191,7 +192,7 @@ order_fill_format AS (
         digest,
         trader,
         subaccount,
-        'v1' as version,
+        'v1' AS version,
         expiration AS expiration_raw,
         utils.udf_int_to_binary(TRY_TO_NUMBER(expiration)) AS exp_binary,
         utils.udf_binary_to_int(SUBSTR(exp_binary, -2)) AS order_type,
@@ -233,7 +234,7 @@ order_fill_format AS (
         _inserted_timestamp
     FROM
         order_fill_decode
-UNION ALL
+    UNION ALL
     SELECT
         block_number,
         block_timestamp,
@@ -249,7 +250,7 @@ UNION ALL
         digest,
         trader,
         subaccount,
-        'v2' as version,
+        'v2' AS version,
         expiration AS expiration_raw,
         utils.udf_int_to_binary(TRY_TO_NUMBER(expiration)) AS exp_binary,
         utils.udf_binary_to_int(SUBSTR(exp_binary, -2)) AS order_type,
@@ -290,7 +291,7 @@ UNION ALL
         _log_id,
         _inserted_timestamp
     FROM
-        order_fill_decode_v2 
+        order_fill_decode_v2
 ),
 FINAL AS (
     SELECT
@@ -308,6 +309,7 @@ FINAL AS (
         digest,
         trader,
         subaccount,
+        version,
         CASE
             WHEN amount < 0 THEN 'sell/short'
             WHEN amount > 0 THEN 'buy/long'
