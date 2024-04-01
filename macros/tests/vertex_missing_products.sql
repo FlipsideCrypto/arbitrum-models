a25425df8d39dc624701dbb93d48da5bc18a5324
mattromano <mattromano88@gmail.com>
2024-04-01 15:15:38 -0700

    add product level rece tests

--- macros/tests/vertex_missing_products.sql
+++ macros/tests/vertex_missing_products.sql
@@ -20,3 +20,26 @@ select *
 from invalid_product_ids
 
 {% endtest %}
+
+{% test vertex_product_level_recency(
+    model,
+    filter) %}
+
+with recent_records as (
+    select distinct(product_id) from  {{model}}
+    where block_timestamp >= SYSDATE() - INTERVAL '1 DAY'
+),
+
+invalid_product_ids as (
+    select *
+    from {{ ref('silver__vertex_dim_products') }}
+    where product_id not in (select product_id from recent_records)
+    {% if filter %}
+        AND {{ filter }}
+    {% endif %}
+)
+
+select * 
+from invalid_product_ids
+
+{% endtest %}
