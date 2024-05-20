8356f7f5aa6cbdaa0b52bef11eaafde8fbc457a0
mattromano <mattromano88@gmail.com>
2024-05-20 10:28:15 -0700

    format

--- models/silver/defi/perps/gmx/v1/silver__gmx_dim_products_v1.sql
+++ models/silver/defi/perps/gmx/v1/silver__gmx_dim_products_v1.sql
@@ -4,29 +4,33 @@
     tags = ['curated']
 ) }}
 
-WITH
-api_pull AS (
+WITH api_pull AS (
+
     SELECT
         PARSE_JSON(
             live.udf_api(
                 'https://api.gmx.io/tokens'
             )
-        ) :data  AS response
+        ) :data AS response
 ),
 api_lateral_flatten AS (
     SELECT
-        r.value:data as value
+        r.value :data AS VALUE
     FROM
         api_pull,
         LATERAL FLATTEN (response) AS r
-)
-,
+),
 product_metadata AS (
     SELECT
-        lower(VALUE :address) AS address,
+        LOWER(
+            VALUE :address
+        ) AS address,
         VALUE :decimals AS decimals,
         VALUE :symbol AS symbol
     FROM
         api_lateral_flatten
 )
-select * from product_metadata
+SELECT
+    *
+FROM
+    product_metadata
