f7ee7ce22f012e373bbc11dd9b2ecf39be7baa4d
drethereum <trevor.wenokur@gmail.com>
2024-05-21 10:51:55 -0600

    heal logic

--- models/silver/defi/lending/complete_lending/silver__complete_lending_withdraws.sql
+++ models/silver/defi/lending/complete_lending/silver__complete_lending_withdraws.sql
@@ -43,7 +43,7 @@ WHERE
     )
 {% endif %}
 ),
-radiant as (
+radiant AS (
     SELECT
         tx_hash,
         block_number,
@@ -66,19 +66,19 @@ radiant as (
     FROM
         {{ ref('silver__radiant_withdraws') }}
 
-    {% if is_incremental() and 'radiant' not in var('HEAL_MODELS') %}
-    WHERE
-        _inserted_timestamp >= (
-            SELECT
-                MAX(
-                    _inserted_timestamp
-                ) - INTERVAL '{{ var("LOOKBACK", "4 hours") }}'
-            FROM
-                {{ this }}
-        )
-    {% endif %}
+{% if is_incremental() and 'radiant' not in var('HEAL_MODELS') %}
+WHERE
+    _inserted_timestamp >= (
+        SELECT
+            MAX(
+                _inserted_timestamp
+            ) - INTERVAL '{{ var("LOOKBACK", "4 hours") }}'
+        FROM
+            {{ this }}
+    )
+{% endif %}
 ),
-comp as (
+comp AS (
     SELECT
         tx_hash,
         block_number,
@@ -101,19 +101,19 @@ comp as (
     FROM
         {{ ref('silver__comp_withdraws') }}
 
-    {% if is_incremental() and 'comp' not in var('HEAL_MODELS') %}
-    WHERE
-        _inserted_timestamp >= (
-            SELECT
-                MAX(
-                    _inserted_timestamp
-                ) - INTERVAL '{{ var("LOOKBACK", "4 hours") }}'
-            FROM
-                {{ this }}
-        )
-    {% endif %}
+{% if is_incremental() and 'comp' not in var('HEAL_MODELS') %}
+WHERE
+    _inserted_timestamp >= (
+        SELECT
+            MAX(
+                _inserted_timestamp
+            ) - INTERVAL '{{ var("LOOKBACK", "4 hours") }}'
+        FROM
+            {{ this }}
+    )
+{% endif %}
 ),
-lodestar as (
+lodestar AS (
     SELECT
         tx_hash,
         block_number,
@@ -136,19 +136,19 @@ lodestar as (
     FROM
         {{ ref('silver__lodestar_withdraws') }}
 
-    {% if is_incremental() and 'lodestar' not in var('HEAL_MODELS') %}
-    WHERE
-        _inserted_timestamp >= (
-            SELECT
-                MAX(
-                    _inserted_timestamp
-                ) - INTERVAL '{{ var("LOOKBACK", "4 hours") }}'
-            FROM
-                {{ this }}
-        )
-    {% endif %}
+{% if is_incremental() and 'lodestar' not in var('HEAL_MODELS') %}
+WHERE
+    _inserted_timestamp >= (
+        SELECT
+            MAX(
+                _inserted_timestamp
+            ) - INTERVAL '{{ var("LOOKBACK", "4 hours") }}'
+        FROM
+            {{ this }}
+    )
+{% endif %}
 ),
-dforce as (
+dforce AS (
     SELECT
         tx_hash,
         block_number,
@@ -171,19 +171,19 @@ dforce as (
     FROM
         {{ ref('silver__dforce_withdraws') }}
 
-    {% if is_incremental() and 'dforce' not in var('HEAL_MODELS') %}
-    WHERE
-        _inserted_timestamp >= (
-            SELECT
-                MAX(
-                    _inserted_timestamp
-                ) - INTERVAL '{{ var("LOOKBACK", "4 hours") }}'
-            FROM
-                {{ this }}
-        )
-    {% endif %}
+{% if is_incremental() and 'dforce' not in var('HEAL_MODELS') %}
+WHERE
+    _inserted_timestamp >= (
+        SELECT
+            MAX(
+                _inserted_timestamp
+            ) - INTERVAL '{{ var("LOOKBACK", "4 hours") }}'
+        FROM
+            {{ this }}
+    )
+{% endif %}
 ),
-silo as (
+silo AS (
     SELECT
         tx_hash,
         block_number,
@@ -206,50 +206,50 @@ silo as (
     FROM
         {{ ref('silver__silo_withdraws') }}
 
-    {% if is_incremental() and 'silo' not in var('HEAL_MODELS') %}
-    WHERE
-        _inserted_timestamp >= (
-            SELECT
-                MAX(
-                    _inserted_timestamp
-                ) - INTERVAL '{{ var("LOOKBACK", "4 hours") }}'
-            FROM
-                {{ this }}
-        )
-    {% endif %}
+{% if is_incremental() and 'silo' not in var('HEAL_MODELS') %}
+WHERE
+    _inserted_timestamp >= (
+        SELECT
+            MAX(
+                _inserted_timestamp
+            ) - INTERVAL '{{ var("LOOKBACK", "4 hours") }}'
+        FROM
+            {{ this }}
+    )
+{% endif %}
 ),
-withdraws as (
-  SELECT
-    *
-  FROM
-    aave
-  UNION ALL
-  SELECT
-    *
-  FROM
-    radiant
-  UNION ALL
-  SELECT
-    *
-  FROM
-    comp
-  UNION ALL
-  SELECT
-    *
-  FROM
-    dforce
-  UNION ALL
-  SELECT
-    *
-  FROM
-    lodestar
-  UNION ALL
-  SELECT
-    *
-  FROM
-    silo
+withdraws AS (
+    SELECT
+        *
+    FROM
+        aave
+    UNION ALL
+    SELECT
+        *
+    FROM
+        radiant
+    UNION ALL
+    SELECT
+        *
+    FROM
+        comp
+    UNION ALL
+    SELECT
+        *
+    FROM
+        dforce
+    UNION ALL
+    SELECT
+        *
+    FROM
+        lodestar
+    UNION ALL
+    SELECT
+        *
+    FROM
+        silo
 ),
-FINAL AS (
+complete_lending_withdraws AS (
     SELECT
         tx_hash,
         block_number,
@@ -287,8 +287,126 @@ FINAL AS (
             'hour',
             block_timestamp
         ) = p.hour
-        LEFT JOIN {{ ref('silver__contracts') }} C
-        ON A.token_address = C.contract_address
+),
+
+{% if is_incremental() and var(
+    'HEAL_MODEL'
+) %}
+heal_model AS (
+    SELECT
+        tx_hash,
+        block_number,
+        block_timestamp,
+        event_index,
+        origin_from_address,
+        origin_to_address,
+        origin_function_signature,
+        t0.contract_address,
+        event_name,
+        protocol_market,
+        depositor,
+        t0.token_address,
+        t0.token_symbol,
+        amount_unadj,
+        amount,
+        ROUND(
+            amount * p.price,
+            2
+        ) AS amount_usd_heal,
+        platform,
+        t0.blockchain,
+        t0._log_id,
+        t0._inserted_timestamp
+    FROM
+        {{ this }}
+        t0
+        LEFT JOIN {{ ref('price__ez_prices_hourly') }}
+        p
+        ON t0.token_address = p.token_address
+        AND DATE_TRUNC(
+            'hour',
+            block_timestamp
+        ) = p.hour
+    WHERE
+        CONCAT(
+            t0.block_number,
+            '-',
+            t0.platform
+        ) IN (
+            SELECT
+                CONCAT(
+                    t1.block_number,
+                    '-',
+                    t1.platform
+                )
+            FROM
+                {{ this }}
+                t1
+            WHERE
+                t1.amount_usd IS NULL
+                AND t1._inserted_timestamp < (
+                    SELECT
+                        MAX(
+                            _inserted_timestamp
+                        ) - INTERVAL '{{ var("LOOKBACK", "4 hours") }}'
+                    FROM
+                        {{ this }}
+                )
+                AND EXISTS (
+                    SELECT
+                        1
+                    FROM
+                        {{ ref('silver__complete_token_prices') }}
+                        p
+                    WHERE
+                        p._inserted_timestamp > DATEADD('DAY', -14, SYSDATE())
+                        AND p.price IS NOT NULL
+                        AND p.token_address = t1.token_address
+                        AND p.hour = DATE_TRUNC(
+                            'hour',
+                            t1.block_timestamp
+                        )
+                )
+            GROUP BY
+                1
+        )
+),
+{% endif %}
+
+FINAL AS (
+    SELECT
+        *
+    FROM
+        complete_lending_withdraws
+
+{% if is_incremental() and var(
+    'HEAL_MODEL'
+) %}
+UNION ALL
+SELECT
+    tx_hash,
+    block_number,
+    block_timestamp,
+    event_index,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature,
+    contract_address,
+    event_name,
+    protocol_market,
+    depositor,
+    token_address,
+    token_symbol,
+    amount_unadj,
+    amount,
+    amount_usd_heal AS amount_usd,
+    platform,
+    blockchain,
+    _log_id,
+    _inserted_timestamp
+FROM
+    heal_model
+{% endif %}
 )
 SELECT
     *,
