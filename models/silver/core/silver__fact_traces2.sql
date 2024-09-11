c2e4c7522473ee301ac770ba411a62eabcb1e954
Austin <93135983+austinFlipside@users.noreply.github.com>
2024-09-11 14:04:38 -0400

    An 4954/regular logic (#262)
    * regular incremental
    
    * regular traces

--- models/silver/core/silver__fact_traces2.sql
+++ models/silver/core/silver__fact_traces2.sql
@@ -4,12 +4,11 @@
     unique_key = ['block_number'],
     cluster_by = "block_timestamp::date",
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION",
-    tags = ['traces_reload'],
+    tags = ['core','non_realtime'],
     full_refresh = false
 ) }}
 {{ fsc_evm.gold_traces_v1(
     full_reload_start_block = 30000000,
     full_reload_blocks = 10000000,
-    full_reload_mode = true,
     arb_traces_mode = true
 ) }}
