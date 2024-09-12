80b1f158dcebe6d8f9308409a1a70ee3ce51dee3
Austin <93135983+austinFlipside@users.noreply.github.com>
2024-09-11 23:26:10 -0400

    Test marco update (#266)
    * stash
    
    * predicates

--- macros/dbt/predicate.sql
+++ macros/dbt/predicate.sql
@@ -1,9 +1,26 @@
-{% macro between_predicate(input_column='block_number') %}
-    {% set database_name = target.database %}
-    {% set schema_name = generate_schema_name(node=model) %}
-    {% set table_name = generate_alias_name(node=model) %}
-    {{- database_name }}.{{ schema_name }}.{{ table_name }}.{{ input_column }} BETWEEN 
-    (SELECT MIN({{ input_column }}) FROM {{ database_name }}.{{ schema_name }}.{{ table_name }}__dbt_tmp)
-    AND 
-    (SELECT MAX({{ input_column }}) FROM {{ database_name }}.{{ schema_name }}.{{ table_name }}__dbt_tmp)
+{% macro standard_predicate(
+        input_column = 'block_number'
+    ) -%}
+    {%- set database_name = target.database -%}
+    {%- set schema_name = generate_schema_name(
+        node = model
+    ) -%}
+    {%- set table_name = generate_alias_name(
+        node = model
+    ) -%}
+    {%- set tmp_table_name = table_name ~ '__dbt_tmp' -%}
+    {%- set full_table_name = (
+        database_name ~ '.' ~ schema_name ~ '.' ~ table_name
+    ) | trim -%}
+    {%- set full_tmp_table_name = (
+        database_name ~ '.' ~ schema_name ~ '.' ~ tmp_table_name
+    ) | trim -%}
+    {{ full_table_name }}.{{ input_column }} >= (
+        SELECT
+            MIN(
+                {{ input_column }}
+            )
+        FROM
+            {{ full_tmp_table_name }}
+    )
 {%- endmacro %}
