7d1dadd2f10a42130ea14c2b4312785919353ad0
Austin <austin@flipsidecrypto.com>
2025-01-29 12:43:43 -0500

    tests

---
+++ tests/arbitrum/test_gold__missing_traces.sql
@@ -0,0 +1,1 @@
+{{ missing_traces(ref("test_silver__transactions_full"), ref("test_gold__fact_traces_full")) }}
--- tests/arbitrum/test_silver__recent_missing_traces.sql
+++
@@ -1,1 +0,0 @@
-{{ missing_traces(ref("test_silver__transactions_recent"), ref("test_silver__traces_recent")) }}
