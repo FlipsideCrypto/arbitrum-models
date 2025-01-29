173a876b2728710b07b59b48721c260fa7236908
Sam <110511194+sam-xyz@users.noreply.github.com>
2025-01-30 02:22:44 +0800

    An 5714/traces2 hotswap arb (#298)
    * change to gold
    
    * docs
    
    * add tests
    
    * tests
    
    ---------
    
    Co-authored-by: Austin <austin@flipsidecrypto.com>

--- models/silver/defi/bridge/stargate/silver_bridge__stargate_createpool.sql
+++ models/silver/defi/bridge/stargate/silver_bridge__stargate_createpool.sql
@@ -13,10 +13,19 @@ WITH base_contracts AS (
         block_timestamp,
         from_address,
         to_address AS contract_address,
-        _call_id,
-        _inserted_timestamp
+        concat_ws(
+            '-',
+            block_number,
+            tx_position,
+            CONCAT(
+                TYPE,
+                '_',
+                trace_address
+            )
+        ) AS _call_id,
+        modified_timestamp AS _inserted_timestamp
     FROM
-        {{ ref('silver__traces') }}
+        {{ ref('core__fact_traces') }}
     WHERE
         from_address = '0x55bdb4164d28fbaf0898e0ef14a589ac09ac9970'
         AND TYPE ILIKE 'create%'
