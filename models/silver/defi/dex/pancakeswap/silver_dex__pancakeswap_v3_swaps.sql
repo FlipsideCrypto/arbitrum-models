611fc265a7d3bd9226014a13c8cadb0c56a3be4b
San Yong <22216004+SanYongxie@users.noreply.github.com>
2025-02-04 21:31:05 +0800

    updated psc v3 to core tables

--- models/silver/defi/dex/pancakeswap/silver_dex__pancakeswap_v3_swaps.sql
+++ models/silver/defi/dex/pancakeswap/silver_dex__pancakeswap_v3_swaps.sql
@@ -104,7 +104,7 @@ base_swaps AS (
         ) AS _log_id,
         l.modified_timestamp
     FROM
-        {{ ref('silver__logs') }} l
+        {{ ref('core__fact_event_logs') }} l
         INNER JOIN pools p
         ON l.contract_address = pool_address
     WHERE
--- models/silver/defi/dex/sushi/silver_dex__sushi_swaps_v3.sql
+++ models/silver/defi/dex/sushi/silver_dex__sushi_swaps_v3.sql
@@ -76,7 +76,6 @@ AND modified_timestamp >= (
 AND modified_timestamp >= SYSDATE() - INTERVAL '7 day'
 {% endif %}
 )
-
 SELECT
     block_number,
     block_timestamp,
@@ -99,20 +98,20 @@ SELECT
     amount0_unadj,
     amount1_unadj,
     CASE
-      WHEN amount0_unadj > 0 THEN ABS(amount0_unadj)
-      ELSE ABS(amount1_unadj)
+        WHEN amount0_unadj > 0 THEN ABS(amount0_unadj)
+        ELSE ABS(amount1_unadj)
     END AS amount_in_unadj,
     CASE
-      WHEN amount0_unadj < 0 THEN ABS(amount0_unadj)
-      ELSE ABS(amount1_unadj)
+        WHEN amount0_unadj < 0 THEN ABS(amount0_unadj)
+        ELSE ABS(amount1_unadj)
     END AS amount_out_unadj,
     CASE
-      WHEN amount0_unadj > 0 THEN token0_address
-      ELSE token1_address
+        WHEN amount0_unadj > 0 THEN token0_address
+        ELSE token1_address
     END AS token_in,
     CASE
-      WHEN amount0_unadj < 0 THEN token0_address
-      ELSE token1_address
+        WHEN amount0_unadj < 0 THEN token0_address
+        ELSE token1_address
     END AS token_out,
     _log_id,
     modified_timestamp
