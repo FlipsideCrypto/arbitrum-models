0de136bd04e65d76dfce605b027efa5a6f4593cf
drethereum <trevor.wenokur@gmail.com>
2025-02-12 13:32:42 -0700

    changes for standardization

--- models/silver/defi/dex/dodo/silver_dex__dodo_v1_swaps.sql
+++ models/silver/defi/dex/dodo/silver_dex__dodo_v1_swaps.sql
@@ -79,7 +79,7 @@ AND _inserted_timestamp >= (
         {{ this }}
 )
 AND _inserted_timestamp >= SYSDATE() - INTERVAL '7 day'
-AND block_timestamp >= SYSDATE() - INTERVAL '7 day'
+
 {% endif %}
 ),
 buy_base_token AS (
@@ -139,7 +139,7 @@ AND _inserted_timestamp >= (
         {{ this }}
 )
 AND _inserted_timestamp >= SYSDATE() - INTERVAL '7 day'
-AND block_timestamp >= SYSDATE() - INTERVAL '7 day'
+
 {% endif %}
 )
 SELECT
