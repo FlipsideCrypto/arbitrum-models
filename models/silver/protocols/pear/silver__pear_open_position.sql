92a09aa9ad4ed48fbce0d36fef7c2c17e0ff8ece
mattromano <mattromano88@gmail.com>
2025-02-18 19:44:45 -0800

    add position open table

---
+++ models/silver/protocols/pear/silver__pear_open_position.sql
@@ -0,0 +1,66 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = ['tx_hash', 'quote_id'],
+    cluster_by = ['block_timestamp::DATE'],
+    tags = ['curated','reorg']
+) }}
+
+with decoded_logs as (
+    SELECT
+        tx_hash,
+        block_number,
+        block_timestamp,
+        origin_from_address,
+        origin_to_address,
+        contract_address,
+        event_name,
+        decoded_log,
+        topic_0,
+        decoded_log:quoteId as quoteId,
+        decoded_log:partyA as partyA,
+        decoded_log:partyB as partyB,
+        decoded_log:openedPrice*pow(10,-18) as openedPrice,
+        decoded_log:filledAmount*pow(10,-18) as filledAmount,
+        filledAmount*openedPrice as tx_vol_usd,
+        modified_timestamp,
+        ez_decoded_event_logs_id
+    FROM
+        arbitrum.core.ez_decoded_event_logs
+    WHERE
+        contract_address = LOWER('0x8f06459f184553e5d04f07f868720bdacab39395')
+        AND topic_0 = '0xa50f98254710514f60327a4e909cd0be099a62f316299907ef997f3dc4d1cda5'
+    {% if is_incremental() %}
+    AND modified_timestamp >= (
+        SELECT
+            MAX(modified_timestamp) - INTERVAL '12 hours'
+        FROM
+            {{ this }}
+    )
+    AND modified_timestamp >= SYSDATE() - INTERVAL '7 day'
+    {% endif %}
+)
+select 
+    l.tx_hash,
+    l.block_number,
+    l.block_timestamp,
+    l.event_name,
+    l.origin_from_address,
+    l.origin_to_address,
+    l.topic_0,
+    quoteId,
+    partyA,
+    partyB,
+    openedPrice,
+    filledAmount,
+    tx_vol_usd,
+    l.modified_timestamp as _inserted_timestamp,
+    l.ez_decoded_event_logs_id,
+    {{ dbt_utils.generate_surrogate_key(
+        ['l.tx_hash', 'l.quote_id']
+    ) }} AS pear_perps_id,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    '{{ invocation_id }}' AS _invocation_id
+FROM
+    decoded_logs l
--- models/silver/protocols/pear/silver__pear_perps.sql
+++ models/silver/protocols/pear/silver__pear_perps.sql
@@ -1,47 +1,45 @@
 {{ config(
     materialized = 'incremental',
     incremental_strategy = 'delete+insert',
-    unique_key = 'product_id',
+    unique_key = ['tx_hash', 'quote_id'],
     cluster_by = ['block_timestamp::DATE'],
     tags = ['curated','reorg']
 ) }}
+
 with decoded_logs as (
     SELECT
         tx_hash,
         block_number,
         block_timestamp,
+        origin_from_address,
+        origin_to_address,
         contract_address,
         event_name,
-        decoded_log as decoded_log,
+        decoded_log,
         topic_0,
-        decoded_log :fundingRateEpochDuration :: INTEGER AS funding_rate_epoch_duration,
-        decoded_log :fundingRateWindowTime :: INTEGER AS funding_rate_window_time,
-        decoded_log :maxLeverage :: DECIMAL(
-            38,
-            0
-        ) / 1e18 AS max_leverage,
-        decoded_log :minAcceptablePortionLF :: DECIMAL(
-            38,
-            0
-        ) / 1e18 AS min_acceptable_portion_lf,
-        decoded_log :minAcceptableQuoteValue :: DECIMAL(
-            38,
-            0
-        ) / 1e18 AS min_acceptable_quote_value,
-        decoded_log :name :: STRING AS NAME,
-        decoded_log :id :: STRING AS id,
-        decoded_log :symbolId :: STRING AS symbol_id,
-        decoded_log :tradingFee :: DECIMAL(
-            38,
-            0
-        ) / 1e18 AS trading_fee,
+        decoded_log:cva::decimal(38,0)/1e18 as cva,
+        decoded_log:deadline::integer as deadline,
+        decoded_log:lf::decimal(38,0)/1e18 as lf,
+        decoded_log:marketPrice::decimal(38,0)/1e18 as market_price,
+        decoded_log:orderType::integer as order_type,
+        decoded_log:partyA::string as party_a,
+        decoded_log:partyAmm::decimal(38,0)/1e18 as party_amm,
+        decoded_log:partyBmm::decimal(38,0)/1e18 as party_bmm,
+        decoded_log:partyBsWhiteList as party_bs_white_list,
+        decoded_log:positionType::integer as position_type,
+        decoded_log:price::decimal(38,0)/1e18 as price,
+        decoded_log:quantity::decimal(38,0)/1e18 as quantity,
+        decoded_log:quoteId::integer as quote_id,
+        decoded_log:symbolId::integer as symbol_id,
+        decoded_log:tradingFee::decimal(38,0)/1e18 as trading_fee,
         modified_timestamp,
         ez_decoded_event_logs_id
     FROM
-        arbitrum.core.ez_decoded_event_logs --switch to ref once dev refresh is done
+        arbitrum.core.ez_decoded_event_logs
     WHERE
         contract_address = LOWER('0x8f06459f184553e5d04f07f868720bdacab39395')
-        AND topic_0 = '0xf688a4e13cb28e5c12cf045718e469d8f189222cc94e8ffd4225f0703da46a72' --add_product
+        {# AND origin_to_address = LOWER('0x6273242a7e88b3de90822b31648c212215caafe4') #}
+        AND topic_0 = '0x8a17f103c77224ce4d9bab74dad3bd002cd24cf88d2e191e86d18272c8f135dd'
     {% if is_incremental() %}
     AND modified_timestamp >= (
         SELECT
@@ -50,32 +48,44 @@ with decoded_logs as (
             {{ this }}
     )
     AND modified_timestamp >= SYSDATE() - INTERVAL '7 day'
-
     {% endif %}
 )
 select 
-    tx_hash,
-    block_number,
-    block_timestamp,
-    event_name,
-    coalesce(id,symbol_id)::INTEGER as product_id,
-    topic_0,
-    funding_rate_epoch_duration,
-    funding_rate_window_time,
-    max_leverage,
-    min_acceptable_portion_lf,
-    min_acceptable_quote_value,
-    NAME as product_name,
-    trading_fee,
-    modified_timestamp as _inserted_timestamp,
-    ez_decoded_event_logs_id,
+    l.tx_hash,
+    l.block_number,
+    l.block_timestamp,
+    l.event_name,
+    l.origin_from_address,
+    l.origin_to_address,
+    l.symbol_id as product_id,
+    l.topic_0,
+    l.cva,
+    l.deadline,
+    l.lf,
+    l.market_price,
+    l.order_type,
+    l.party_a,
+    l.party_amm,
+    l.party_bmm,
+    l.party_bs_white_list,
+    l.position_type,
+    l.price,
+    l.quantity,
+    l.quote_id,
+    l.trading_fee,
+    p.product_name,
+    l.modified_timestamp as _inserted_timestamp,
+    l.ez_decoded_event_logs_id,
     {{ dbt_utils.generate_surrogate_key(
-        ['tx_hash','product_id']
-    ) }} AS pear_products_id,
+        ['l.tx_hash', 'l.quote_id']
+    ) }} AS pear_perps_id,
     SYSDATE() AS inserted_timestamp,
     SYSDATE() AS modified_timestamp,
     '{{ invocation_id }}' AS _invocation_id
 FROM
-    decoded_logs qualify(ROW_NUMBER() over(PARTITION BY product_id
+    decoded_logs l
+    LEFT JOIN {{ ref('silver__pear_dim_products') }} p
+        ON l.symbol_id = p.product_id
+qualify(ROW_NUMBER() over(PARTITION BY l.quote_id
 ORDER BY
     _inserted_timestamp DESC)) = 1
