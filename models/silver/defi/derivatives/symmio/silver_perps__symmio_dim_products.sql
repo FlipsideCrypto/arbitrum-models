1099e21ab28d90b73927ecbe14c81d442974d957
San Yong <22216004+SanYongxie@users.noreply.github.com>
2025-03-05 16:43:45 +0800

    update symmio products model to fact logs

--- models/silver/defi/derivatives/symmio/silver_perps__symmio_dim_products.sql
+++ models/silver/defi/derivatives/symmio/silver_perps__symmio_dim_products.sql
@@ -1,81 +1,128 @@
 {{ config(
     materialized = 'incremental',
     incremental_strategy = 'delete+insert',
-    unique_key = 'product_id',
+    unique_key = '_log_id',
     cluster_by = ['block_timestamp::DATE'],
     tags = ['curated','reorg']
 ) }}
-with decoded_logs as (
+
+WITH logs AS (
+
     SELECT
         tx_hash,
         block_number,
         block_timestamp,
         contract_address,
-        event_name,
-        decoded_log as decoded_log,
+        origin_from_address,
+        origin_to_address,
+        origin_function_signature,
+        event_index,
+        topics,
+        'AddSymbol' AS event_name,
         topic_0,
-        decoded_log :fundingRateEpochDuration :: INTEGER AS funding_rate_epoch_duration,
-        decoded_log :fundingRateWindowTime :: INTEGER AS funding_rate_window_time,
-        decoded_log :maxLeverage :: DECIMAL(
-            38,
-            0
-        ) / 1e18 AS max_leverage,
-        decoded_log :minAcceptablePortionLF :: DECIMAL(
-            38,
-            0
-        ) / 1e18 AS min_acceptable_portion_lf,
-        decoded_log :minAcceptableQuoteValue :: DECIMAL(
-            38,
-            0
-        ) / 1e18 AS min_acceptable_quote_value,
-        decoded_log :name :: STRING AS NAME,
-        decoded_log :id :: STRING AS id,
-        decoded_log :symbolId :: STRING AS symbol_id,
-        decoded_log :tradingFee :: DECIMAL(
-            38,
-            0
-        ) / 1e18 AS trading_fee,
-        modified_timestamp,
-        ez_decoded_event_logs_id
+        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
+        TRY_TO_NUMBER(
+            arbitrum.utils.udf_hex_to_int(
+                segmented_data [0] :: STRING
+            )
+        ) AS symbol_id,
+        arbitrum.utils.udf_hex_to_string(
+            segmented_data [9] :: STRING
+        ) AS NAME,
+        TRY_TO_NUMBER(
+            arbitrum.utils.udf_hex_to_int(
+                segmented_data [2] :: STRING
+            )
+        ) * pow(
+            10,
+            -18
+        ) AS min_acceptable_quote_value,
+        TRY_TO_NUMBER(
+            arbitrum.utils.udf_hex_to_int(
+                segmented_data [3] :: STRING
+            )
+        ) * pow(
+            10,
+            -18
+        ) AS min_acceptable_portion_lf,
+        TRY_TO_NUMBER(
+            arbitrum.utils.udf_hex_to_int(
+                segmented_data [4] :: STRING
+            )
+        ) * pow(
+            10,
+            -18
+        ) AS trading_fee,
+        TRY_TO_NUMBER(
+            arbitrum.utils.udf_hex_to_int(
+                segmented_data [5] :: STRING
+            )
+        ) * pow(
+            10,
+            -18
+        ) AS max_leverage,
+        TRY_TO_NUMBER(
+            arbitrum.utils.udf_hex_to_int(
+                segmented_data [6] :: STRING
+            )
+        ) AS funding_rate_epoch_duration,
+        TRY_TO_NUMBER(
+            arbitrum.utils.udf_hex_to_int(
+                segmented_data [7] :: STRING
+            )
+        ) AS funding_rate_window_time,
+        CONCAT(
+            tx_hash,
+            '-',
+            event_index
+        ) AS _log_id,
+        modified_timestamp
     FROM
-        {{ ref('core__ez_decoded_event_logs') }}
+        {{ ref('core__fact_event_logs') }}
     WHERE
         contract_address = LOWER('0x8f06459f184553e5d04f07f868720bdacab39395')
         AND topic_0 = '0xf688a4e13cb28e5c12cf045718e469d8f189222cc94e8ffd4225f0703da46a72' --add_product
-    {% if is_incremental() %}
-    AND modified_timestamp >= (
-        SELECT
-            MAX(modified_timestamp) - INTERVAL '12 hours'
-        FROM
-            {{ this }}
-    )
-    AND modified_timestamp >= SYSDATE() - INTERVAL '7 day'
+        AND tx_succeeded
 
-    {% endif %}
+{% if is_incremental() %}
+AND modified_timestamp >= (
+    SELECT
+        MAX(modified_timestamp) - INTERVAL '12 hours'
+    FROM
+        {{ this }}
 )
-select 
-    tx_hash,
+AND modified_timestamp >= SYSDATE() - INTERVAL '7 day'
+{% endif %}
+)
+SELECT
     block_number,
     block_timestamp,
+    tx_hash,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature,
+    contract_address,
+    event_index,
+    topics,
     event_name,
-    coalesce(id,symbol_id)::INTEGER as product_id,
+    symbol_id AS product_id,
     topic_0,
     funding_rate_epoch_duration,
     funding_rate_window_time,
     max_leverage,
     min_acceptable_portion_lf,
     min_acceptable_quote_value,
-    NAME as product_name,
+    NAME AS product_name,
     trading_fee,
-    modified_timestamp as _inserted_timestamp,
-    ez_decoded_event_logs_id,
+    modified_timestamp AS _inserted_timestamp,
+    _log_id,
     {{ dbt_utils.generate_surrogate_key(
         ['tx_hash','product_id']
-    ) }} AS pear_products_id,
+    ) }} AS symmio_products_id,
     SYSDATE() AS inserted_timestamp,
     SYSDATE() AS modified_timestamp,
     '{{ invocation_id }}' AS _invocation_id
 FROM
-    decoded_logs qualify(ROW_NUMBER() over(PARTITION BY product_id
+    logs qualify(ROW_NUMBER() over(PARTITION BY product_id
 ORDER BY
     _inserted_timestamp DESC)) = 1
